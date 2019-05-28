6e42a0a98a9373e6d59ba93e0343c169eca8c606
Jay Maas <jaymaas.dev@gmail.com>
2019-05-28 15:46:43 -0600

    Initialized Project and GET req
    Project has been initialized. The GET request to the specified endponit /api/users has been implemented and tested

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// implement your API here
+const express = require('express')
+
+const db = require('./data/db.js')
+
+const server = express() 
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('Hello, welcome to the server')
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The users information could not be retrieved.",
+                err
+            })
+        })
+})
+
+server.listen(5000, () => 
+    console.log('Server running on http://localhost:5000')
+)
