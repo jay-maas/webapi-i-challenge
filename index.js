c94ce4aaa9517cbd674d92e2e1bb4d7340855aa8
Jay Maas <jaymaas.dev@gmail.com>
2019-05-28 18:05:10 -0600

    MVP
    Implemented PUT, POST, DELETE functionality.

--- index.js
+++ index.js
@@ -23,6 +23,99 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } =req.params
+
+    db.findById(id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist." 
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'The user information could not be retrieved.',
+                err
+            })
+        })
+})  
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body
+    const { name, bio } = userInfo
+    if(name && bio) {
+        db.insert(userInfo)
+            .then(userId => {
+                res.status(201).json(userId)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "There was an error while saving the user to the database",
+                    err
+                })
+            })
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."})
+    }
+})
+
+server.delete('/api/users/:id', (req, res) =>{
+    const { id } = req.params
+
+    db.remove(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(204).end()
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The user could not be removed",
+                err
+            })
+        })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    const { name, bio } = changes
+
+    db.update(id, changes)
+        .then(updated => {
+            if(updated) {
+                if(name && bio) {
+                    res.status(200).json(updated)
+                } else {
+                    res.status(400).json({
+                        errorMessage: "Please provide name and bio for the user."
+                    })
+                }
+                
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                })
+            }
+            
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The user information could not be modified.",
+                err
+            })
+        })
+})
+
 server.listen(5000, () => 
     console.log('Server running on http://localhost:5000')
 )
